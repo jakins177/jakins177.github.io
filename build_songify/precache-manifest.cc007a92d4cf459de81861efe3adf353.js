25d79bb274c82780fc9738ef2091039424daa792
Student <student@PSXGA125s-MacBook-Air.local>
2020-03-03 09:33:11 -0500

    pushing songify

---
+++ build_songify/precache-manifest.cc007a92d4cf459de81861efe3adf353.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d55754a9a4935551dd3",
+    "url": "/static/css/main.4e79f07d.chunk.css"
+  },
+  {
+    "revision": "8d55754a9a4935551dd3",
+    "url": "/static/js/main.f0ed0125.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "66b8620f94289493b04b",
+    "url": "/static/js/2.d6d2c28c.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "27490ffaf219e42322e29ec488beb751",
+    "url": "/index.html"
+  }
+];
